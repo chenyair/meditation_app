f8f74f1e5159df8acbc43ffc6557e4b6d71ebfa4
Yair Yair <u8706749@bsmch.net>
2021-06-12 20:32:54 +0300

    delete answer for student

--- lib/main.dart
+++ lib/main.dart
@@ -12,27 +12,10 @@ class MeditationApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Meditation App',
-      theme: _buildMeditationTheme(),
-      home: HomeScreen(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(),
     );
-  }
-
-  ThemeData _buildMeditationTheme() {
-    final ThemeData base = ThemeData.light();
-    return base.copyWith(
-      scaffoldBackgroundColor: Color(0xFfF5F5F5),
-      textTheme: _buildMeditationTextTheme(base.textTheme),
-      primaryTextTheme: _buildMeditationTextTheme(base.primaryTextTheme),
-    );
-  }
-
-  TextTheme _buildMeditationTextTheme(TextTheme base) {
-    return base.copyWith(
-        bodyText1: base.bodyText1!.copyWith(
-          color: Colors.black,
-          fontWeight: FontWeight.bold,
-        ),
-        subtitle1: base.subtitle1!
-            .copyWith(fontWeight: FontWeight.w500, fontSize: 14));
   }
 }
